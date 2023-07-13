56d0fb706b02df16720158da2f1db600c4ab43b6
Evolt404 <jaelanimaulana7@gmail.com>
2023-07-13 13:24:18 +0700

    fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link"
 
 export default function Footer() {
-    return(
+    return (
         <footer className="font-inter flex flex-col items-center bg-[#E7E7E7] p-7">
             <p>Reserne now</p>
             <div className="flex">
-                <a href="https://linktr.ee/sir.loin" className="p-5 m-5 bg-black text-white">Sir Loin</a>
-                <a href="https://linktr.ee/si.lo" className="p-5 m-5 bg-black text-white">Silo</a>
+                <a href="https://linktr.ee/sir.loin" className="p-5 m-5 bg-black text-white flex-grow">Sir Loin</a>
+                <a href="https://linktr.ee/si.lo" className="p-5 m-5 bg-black text-white flex-grow">Silo</a>
             </div>
+
             <hr className="w-full h-[2px] my-5 bg-black" />
             <p className="p-10">&#169; 2023 <span className="font-bold">10/10</span></p>
         </footer>
